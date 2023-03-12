580df7151db91e4043a1d7cae5dd500ea7f4d939
Jay Itkal <itkaljay63@gmail.com>
2023-03-13 00:31:13 +0530

    update

--- portfolio.js
+++ portfolio.js
@@ -30,6 +30,10 @@ let tech_stacks = [
     {
         "teck_name": "VS code",
         "img_link": "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png"
+    },
+    {
+        "teck_name": "Reveal.js",
+        "img_link": "https://d4.alternativeto.net/Vn2obsd-jGESKmORvmaLfjUrAS6iCUKUEolVz0dFals/rs:fill:280:280:0/g:ce:0:0/YWJzOi8vZGlzdC9pY29ucy9yZXZlYWwtanNfMjA2MjEyLnBuZw.png"
     }
 
 ]
@@ -51,3 +55,86 @@ tech_stacks.forEach(tech => {
 
     techStackDiv.append(comp);
 })
+
+
+let project_details = [
+    {
+        "project_name": "Gitlab Onboarding",
+        "details": "This was a collaborative project developed within 2 days, with a team of 2 members. I worked on Coding for building slides.",
+        "tech_stack": "Reveal.js, github",
+        "data_base": "Rveal.js",
+        "banner": "Images/GITLAB.png",
+        "deployed": "https://timely-palmier-635908.netlify.app",
+        "gitlink": "https://github.com/Jay1631/Gitlab-customer-onbording"
+    },
+
+    {
+        "project_name": "Pharmeasy.in Clone",
+        "details": "The project was a individual project done within 5 days.",
+        "tech_stack": "HTML, CSS, JavaScript, github",
+        "data_base": "Local Storaage",
+        "banner": "Images/pharmeasy.png",
+        "deployed": "https://handsome-ball-4549.netlify.app/index.html",
+        "gitlink": "https://github.com/Jay1631/handsome-ball-4549"
+    },
+
+    {
+        "project_name": "JEFIT.com Clone",
+        "details": "A collaborative project completed within 5 days with 5 other colleagues. I've built Exercise page with API Integration.",
+        "tech_stack": "HTML, CSS, JavaScript, github",
+        "data_base": "Json server",
+        "banner": "Images/JEFIT.png",
+        "deployed": "https://effulgent-horse-009694.netlify.app",
+        "gitlink": "https://github.com/Golugauraw1/onerous-train-1093"
+    },
+];
+
+
+project_details.forEach(project => {
+
+    let Projects_Div = document.getElementById("projects_div");
+
+    let Maindiv = document.createElement("div");
+    Maindiv.className = "main"
+
+    let pictureDiv = document.createElement("div");
+    pictureDiv.className = "picture";
+
+    let laptopDiv = document.createElement("div");
+    let screenDiv = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = project.banner;
+    screenDiv.append(img);
+    laptopDiv.append(screenDiv);
+    let base = document.createElement("div");
+    let lower_base = document.createElement("div");
+
+    pictureDiv.append(laptopDiv, base, lower_base);
+
+    let descriptionDiv = document.createElement("div");
+    descriptionDiv.className = "project_description";
+
+    let nameDiv = document.createElement("div");
+    nameDiv.innerText = project.project_name;
+    let projectDetails = document.createElement("div");
+    projectDetails.innerHTML = `<p>${project.details}</p><p>Tech Stack : ${project.tech_stack}</p><p>Database : ${project.data_base}</p>`;
+    let buttonsDiv = document.createElement("div");
+    let deployedBtn = document.createElement("button");
+    deployedBtn.innerText = "Live";
+
+    deployedBtn.onclick = () => {
+        window.open(project.deployed, "_blank");
+    }
+
+    let gitBtn = document.createElement("button");
+    gitBtn.innerText = "GitHub";
+    gitBtn.onclick = () => {
+        window.open(project.gitlink, "_blank");
+    }
+
+    buttonsDiv.append(deployedBtn, gitBtn);
+    descriptionDiv.append(nameDiv, projectDetails, buttonsDiv);
+
+    Maindiv.append(pictureDiv, descriptionDiv);
+    Projects_Div.append(Maindiv);
+});
